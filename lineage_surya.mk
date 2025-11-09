b42da06dd3d684f80197815a5a0677ec8aa6ce86
alvesdeveloperz <miuidroidpessoal@gmail.com>
2025-11-09 17:57:49 -0300

    bringup Lunaris-AOSP dependencies

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -14,12 +14,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# SuperiorOs build flag
+# Lunaris-AOSP flags
+TARGET_OPTIMIZED_DEXOPT := true
 TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+TARGET_USES_CORE_GAPPS := true
+WITH_BCR := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := false
 
-PRODUCT_NAME := superior_surya
+PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
